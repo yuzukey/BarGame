83722415172e64550485a9e2f94fb7c3dc811302
soh <syana000@gmail.com>
2015-03-16 05:18:43 +0900

    Unityちゃんの動きを実装 背景の設定 ジャンプの実装の途中

---
+++ Assets/UnityChan/Scripts/UnityChanContoll.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanContoll : MonoBehaviour {
+
+    private Animator animator;
+    private CharacterController Controller;
+    public bool isGround = false;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        Controller = (CharacterController)GetComponent("CharacterController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKey("up")) {
+            transform.position += transform.forward * 0.05f;
+            animator.SetBool("is_running", true);
+        } else {
+            animator.SetBool("is_running", false);
+        }
+
+        if (Input.GetKey("space") && isGround)
+        {
+            transform.position += transform.up * 0.01f;
+            animator.SetBool("is_jamping", true);
+            isGround = false;
+        }
+        else
+        {
+            animator.SetBool("is_jamping", false);
+        }
+
+        if (Input.GetKey("right")) {
+            transform.Rotate(0, 10, 0);
+        }
+        if (Input.GetKey ("left")) {
+            transform.Rotate(0, -10, 0);
+        }
+	}
+
+    private void OnCollisionEnter(CharacterController Controller)
+    {
+        if (Controller.collider.gameObject.name == "Floor")
+        {
+            isGround = true;
+        }
+    }
+}
