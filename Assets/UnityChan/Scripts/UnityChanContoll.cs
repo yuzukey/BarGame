95efdb4bd9c3c331df616292cd383be2208deda4
yuzukey_note <syana000@gmai.com>
2015-03-16 16:56:19 +0900

    インデントの修正

--- Assets/UnityChan/Scripts/UnityChanContoll.cs
+++ Assets/UnityChan/Scripts/UnityChanContoll.cs
@@ -28,15 +28,14 @@ public class UnityChanContoll : MonoBehaviour {
             transform.position += transform.up * 0.01f;
             animator.SetBool("is_jamping", true);
             isGround = false;
-        }
-        else
-        {
+        } else {
             animator.SetBool("is_jamping", false);
         }
 
         if (Input.GetKey("right")) {
             transform.Rotate(0, 10, 0);
         }
+
         if (Input.GetKey ("left")) {
             transform.Rotate(0, -10, 0);
         }
