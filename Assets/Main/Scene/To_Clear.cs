71bc0fa492f2346942d71c1c2b50f929f0c74d4e
soh <syana000@gmail.com>
2015-03-27 18:51:51 +0900

    クリア判定の設置 ゲーム画面からクリア画面の遷移の実装

---
+++ Assets/Main/Scene/To_Clear.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class To_Clear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Clear"))
+        {
+            Application.LoadLevel(2);
+        }
+    }
+}
