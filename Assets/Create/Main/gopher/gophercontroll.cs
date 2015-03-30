bad2640b0b8cdf692561dbec900afef2c2dc705d
soh <syana000@gmail.com>
2015-03-30 13:12:42 +0900

    gopherが存在していなかったので追加 ゲームの微調整(gopherのジャンプ力、スピード)

---
+++ Assets/Create/Main/gopher/gophercontroll.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gophercontroll : MonoBehaviour 
+{
+
+    public bool isGround = true; 
+    
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * 0.09f;
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.position -= transform.forward * 0.09f;
+        }
+
+
+        if (Input.GetKey("space") && isGround)
+        {
+            rigidbody.AddForce(Vector3.up * 60);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(3, 0, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(-3, 0, 0);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        isGround = true;
+    }
+
+    private void OnCollisionExit(Collision collisionInfo)
+    {
+        isGround = false;
+    }
+}
